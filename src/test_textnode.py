f22d018216651ef2dab49596519c0f606b4aa8e6
rchrd <26688060+rchrd-0@users.noreply.github.com>
2024-07-26 17:40:53 +0800

    feat: TextNode unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,48 @@
+# type: ignore
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    SAMPLE_TEXT = "Foobar"
+
+    def test_eq(self):
+        node_0 = TextNode(self.SAMPLE_TEXT, TextType.bold)
+        node_1 = TextNode("Foobar", TextType.bold)
+        self.assertEqual(node_0, node_1)
+
+    def test_not_eq(self):
+        node_0 = TextNode(self.SAMPLE_TEXT, TextType.bold)
+        node_1 = TextNode(self.SAMPLE_TEXT, TextType.italic)
+        self.assertNotEqual(node_0, node_1)
+
+    def test_default_url(self):
+        node = TextNode(self.SAMPLE_TEXT, TextType.bold)
+        self.assertIsNone(node.url)
+
+    def test_custom_url(self):
+        custom_url = "https://rchrd.co"
+        node = TextNode(self.SAMPLE_TEXT, TextType.bold, custom_url)
+        self.assertIsNotNone(node.url)
+        self.assertEqual(node.url, "https://rchrd.co")
+
+    def test_invalid_text_type_int(self):
+        with self.assertRaises(TypeError):
+            TextNode(self.SAMPLE_TEXT, 1)
+
+    def test_invalid_text_type_str(self):
+        with self.assertRaises(TypeError):
+            TextNode(self.SAMPLE_TEXT, "foo")
+
+    def test_invalid_text_type_attribute(self):
+        with self.assertRaises(AttributeError):
+            TextNode(self.SAMPLE_TEXT, TextType.fail)
+
+    def test_repr(self):
+        expected = "TextNode(Foobar, italic, https://rchrd.co)"
+        node_0 = TextNode(self.SAMPLE_TEXT, TextType.italic, "https://rchrd.co")
+        self.assertEqual(repr(node_0), expected)
+
+    if __name__ == "__main__":
+        unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -7,6 +7,10 @@ TextType = Enum("TextType", ["bold", "italic", "anchor"])
 class TextNode:
     def __init__(self, text: str, text_type: TextType, url: Optional[str] = None):
         self.text = text
+
+        if not isinstance(text_type, TextType):
+            raise TypeError(f"Invalid text_type: {text_type}")
+      
         self.text_type = text_type
         self.url = url
 
@@ -14,4 +18,4 @@ class TextNode:
         return self.__dict__ == other.__dict__
 
     def __repr__(self):
-        return f"{type(self).__name__}({self.text}, {self.text_type}, {self.url})"
+        return f"{type(self).__name__}({self.text}, {self.text_type.name}, {self.url})"
