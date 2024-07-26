ba50ea092f78ce6435456ff5b4cd1c7bd922b915
rchrd <26688060+rchrd-0@users.noreply.github.com>
2024-07-26 23:22:09 +0800

    feat: unit tests for LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,14 +1,11 @@
 # type: ignore
 
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    SAMPLE_PROPS = {
-        "href": "https://rchrd.co",
-        "target": "_blank"
-    }
+    SAMPLE_PROPS = {"href": "https://rchrd.co", "target": "_blank"}
 
     def test_values(self):
         node = HTMLNode("p", "foobar")
@@ -17,14 +14,16 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIsNone(node.children)
         self.assertIsNone(node.props)
 
-    def test_to_html(self):
+    def test_props_to_html(self):
         node = HTMLNode(props=self.SAMPLE_PROPS)
         expected = ' href="https://rchrd.co" target="_blank"'
         self.assertEqual(node.props_to_html(), expected)
 
     def test_to_html_leading_whitespace(self):
         node = HTMLNode(props=self.SAMPLE_PROPS)
-        self.assertNotEqual(node.props_to_html(), 'href="https://rchrd.co" target="_blank"')
+        self.assertNotEqual(
+            node.props_to_html(), 'href="https://rchrd.co" target="_blank"'
+        )
 
     def test_to_html_empty(self):
         node = HTMLNode()
@@ -36,5 +35,36 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(repr(node), expected)
 
 
-if __name__ == '__main__':
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node_0 = LeafNode("p", "This is a paragraph of text.")
+        node_1 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+
+        self.assertEqual(node_0.to_html(), "<p>This is a paragraph of text.</p>")
+        self.assertEqual(
+            node_1.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_repr(self):
+        node_0 = LeafNode("p", "This is a paragraph of text.")
+        node_1 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            repr(node_0), "LeafNode(p, This is a paragraph of text., None, None)"
+        )
+        self.assertEqual(
+            repr(node_1),
+            "LeafNode(a, Click me!, None, {'href': 'https://www.google.com'})",
+        )
+
+    def test_no_tag(self):
+        node_0 = LeafNode(None, "This has no tag")
+        self.assertEqual(node_0.to_html(), "This has no tag")
+
+    def test_none_value(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode("p", None)
+            node.to_html()
+
+
+if __name__ == "__main__":
     unittest.main()
